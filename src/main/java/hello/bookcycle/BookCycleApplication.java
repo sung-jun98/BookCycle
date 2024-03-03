1590f178b6b0d34af6ba96c4931c096afff3bea1
김성준 <sjkim2243@gmail.com>
2024-03-03 19:49:25 +0900

    first commit
    Initiallizer로 프로젝트 생성

---
+++ src/main/java/hello/bookcycle/BookCycleApplication.java
@@ -0,0 +1,13 @@
+package hello.bookcycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCycleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookCycleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/bookcycle/BookCycleApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.bookcycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCycleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
